d96f487a42caaeb05a6dbd42d0a3815a275c6d6b
Arvin Malaluan <75754666+amalaluan@users.noreply.github.com>
2024-10-31 17:13:59 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-gray-900 h-[100dvh] w-[100dvw]'>
+      <div className='px-4 max-w-[400px] m-auto flex flex-col justify-center h-full'>
+        <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZnhvanNseTF6NmN3anh5NjN0c3M1d3NncmZjYjc1N3l3MW1wNGIyZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/W5aG2ZXhxF4l0Z9Iha/giphy.gif" className='max-h-[400px] max-w-[400px] mb-8' alt="" />
+        <p className='text-white md:text-4xl text-2xl text-right md:text-center'>Happy Halloween Milove</p>
+        <p className='text-sm text-right mt-4 italic text-white'>Dalawin mo sana ang puso kong patay na patay sa'yo hehe</p>
+        <p className='text-sm text-right text-white font-bold'>- Arvin</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
